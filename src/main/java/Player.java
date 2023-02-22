38c2c21f166692fb1c4b2eeb80879e59d8b555c2
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-22 18:51:38 +0000

    Complete constructor assignment

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,7 +31,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        this.playerName = playerName;
+        this.level = 1;
     }
 
     /**
@@ -48,7 +49,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName = playerName;
+        this.level = level;
     }
 
 }
